feb6ff5b25c5358ddb23b070e58d7c903b50c533
arturoneill <mobius0919@gmail.com>
2017-11-04 22:13:31 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,6 +18,7 @@ var allCategoriesUrl =
   "https://davids-restaurant.herokuapp.com/categories.json";
 var categoriesTitleHtml = "snippets/restaurants-title-snippet.html";
 var categoryHtml = "snippets/restaurants-snippet.html";
+var errandsHtml = "snippets/errand-snippet.html";
 var menuItemsUrl =
   "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
 var menuItemsTitleHtml = "snippets/menu-items-title.html";
